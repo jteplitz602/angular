1d921b24042a03b254dd7fc38403d14e80990586
Jason Teplitz <jteplitz602@gmail.com>
2015-07-30 16:08:18 -0700

    Add test for WebWorker Todo

--- modules/examples/e2e_test/web_workers/todo/todo_spec.ts
+++ modules/examples/e2e_test/web_workers/todo/todo_spec.ts
@@ -13,7 +13,47 @@ describe('WebWorkers Todo', function() {
     expect(element(by.css("#todoapp header")).getText()).toEqual("todos");
   });
 
+  it('should add a task', () => {
+    browser.get(URL);
+    var taskText = "Test task";
+
+    waitForBootstrap();
+    addTask(taskText);
+    var listItem = element.all(by.css("#todo-list li")).first();
+    var label = listItem.element(by.css("label"));
+    expect(label.getText()).toEqual(taskText);
+  });
+
+  it('should clear completed', () => {
+    browser.get(URL);
+
+    waitForBootstrap();
+    addTask("Task 1");
+    addTask("Task 2");
+    expect(element.all(by.css("#todo-list li")).count()).toEqual(2);
+
+    var checkbox = element.all(by.css("#todo-list li")).first().element(by.css("input.toggle"));
+    checkbox.click();
+    element(by.css("#clear-completed")).click();
+    var condition = new protractor.until.Condition<boolean>("Task didn't get cleared", (driver) => {
+      return element.all(by.css("#todo-list li")).count().then((count) => { return count == 1; });
+    });
+
+    var label = element.all(by.css("#todo-list li")).first().element(by.css("label"));
+    expect(label.getText()).toEqual("Task 2");
+  });
 });
+
+function addTask(taskText: string): void {
+  var todoInput = element(by.css("#new-todo"));
+
+  element(by.css("#new-todo")).sendKeys(protractor.Key.ENTER);
+
+  todoInput.sendKeys(taskText);
+  todoInput.sendKeys(protractor.Key.ENTER);
+
+  browser.wait(protractor.until.elementLocated(by.css("#todo-list li")), 5000);
+}
 
 function waitForBootstrap(): void {
   browser.wait(protractor.until.elementLocated(by.css("todo-app #todoapp")), 5000);
